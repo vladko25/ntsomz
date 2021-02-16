68b13d54bf13b917c600fda045f2b0f45ddd5cbd
Vlad Koronchik <vladislavkoronchik@gmail.com>
2021-02-16 16:44:01 +0300

    16.02.2021 16:43

--- #sourse/js/main.js
+++ #sourse/js/main.js
@@ -1,2 +1,16 @@
 //@prepros-append burger-menu.js
 //@prepros-append change-page-title.js
+new Swiper('.top-posts__swiper', {
+	loop: true,
+	slidesPerView: 1,
+	autoplay: true,
+	speed: 500,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	keyboard: {
+		enabled: true,
+		onlyInViewport: false,
+	},
+});
--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,4 +1,17 @@
-
+new Swiper('.top-posts__swiper', {
+	loop: true,
+	slidesPerView: 1,
+	autoplay: true,
+	speed: 500,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	keyboard: {
+		enabled: true,
+		onlyInViewport: false,
+	},
+});
 /*-----------------------*/
 /*------MENU BURGER------*/
 /*-----------------------*/
